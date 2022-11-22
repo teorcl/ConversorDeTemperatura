ef176ebab6413f3885e01e047afbbcefa9160fc4
Teodoro <teo.calle.lara@gmail.com>
2022-11-21 23:02:51 -0500

    Se agrega funcionalidad de conversión

--- ConversorDeTemperatura/ViewController.swift
+++ ConversorDeTemperatura/ViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private struct Const{
+        static let offset = 32.0
+        static let conversionFactor = 1.8
+    }
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var convertButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var temperatureTextField: UITextField!
+    
+    var resultFahrenheit: Double = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpUI()
+    }
+    
+    @IBAction func convertButtonPressed(_ sender: Any) {
+        convertTempertature()
+    }
+    
+    private func convertTempertature(){
+        let celcius = temperatureTextField.text ?? "0"
+        guard let celciusTurned = Double(celcius) else { return }
+        setUpResultLabel(fahrenheit: convertCelciusToFahrenheit(celcius: celciusTurned))
+    }
+    
+    private func convertCelciusToFahrenheit(celcius: Double) -> Double{
+        return (celcius*Const.conversionFactor)+Const.offset
+    }
+    
+    private func setUpUI(){
+        setUpLogo()
+        setUpButton()
     }
 
+    private func setUpLogo(){
+        logoImageView.clipsToBounds = true
+        logoImageView.layer.cornerRadius = logoImageView.frame.height/2
+    }
+    
+    private func setUpButton(){
+        convertButton.layer.cornerRadius = 12
+    }
+    
+    private func setUpResultLabel(fahrenheit: Double){
+        resultLabel.text = "\(fahrenheit) ºF"
+    }
 
 }
 
